3554bdded4065dbdc4dfb7cdeb5c44769913affd
ssj71 <ssjackson71@gmail.com>
2019-02-09 23:10:54 -0700

    update pugl

--- tikloo/pugl/pugl_x11.c
+++ tikloo/pugl/pugl_x11.c
@@ -35,8 +35,8 @@
 #endif
 
 #ifdef PUGL_HAVE_CAIRO
-#include <cairo/cairo.h>
 #include <cairo/cairo-xlib.h>
+#include <cairo/cairo.h>
 #endif
 
 #include "pugl/cairo_gl.h"
@@ -510,8 +510,9 @@ translateEvent(PuglView* view, XEvent xevent)
 			case 6: event.scroll.dx = -1.0f; break;
 			case 7: event.scroll.dx =  1.0f; break;
 			}
+			// fallthru
 		}
-		// nobreak
+		// fallthru
 	case ButtonRelease:
 		if (xevent.xbutton.button < 4 || xevent.xbutton.button > 7) {
 			event.button.type   = ((xevent.type == ButtonPress)
